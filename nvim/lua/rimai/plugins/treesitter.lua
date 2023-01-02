74ca8eafa78151505d01d4ed1833f1c9a31459a8
Remy de Jong <rimai4@hotmail.com>
2023-01-02 17:57:02 +0100

    enable treesitter indent

--- nvim/lua/rimai/plugins/treesitter.lua
+++ nvim/lua/rimai/plugins/treesitter.lua
@@ -18,7 +18,7 @@ require("nvim-treesitter.configs").setup({
     additional_vim_regex_highlighting = false,
   },
   indent = {
-    enable = false,
+    enable = true,
   },
   endwise = {
     enable = true,

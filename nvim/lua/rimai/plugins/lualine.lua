784d9dc9796c9ee9f5db2abdd786d22af6edfa13
Remy de Jong <rimai4@hotmail.com>
2023-01-03 18:06:00 +0100

    add sonokai lualine theme

--- nvim/lua/rimai/plugins/lualine.lua
+++ nvim/lua/rimai/plugins/lualine.lua
@@ -1,7 +1,5 @@
-local custom_gruvbox = require("lualine.themes.gruvbox")
-
 require("lualine").setup({
-	options = { theme = custom_gruvbox },
+	options = { theme = 'sonokai' },
 	sections = {
 		lualine_a = { "mode" },
 		lualine_b = { "filename" },

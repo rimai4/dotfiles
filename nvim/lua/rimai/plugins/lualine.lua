45f5aad123991bd9cf7be6680758b133ab906131
Remy de Jong <rimai4@hotmail.com>
2023-01-09 10:07:42 +0100

    lualine update

--- nvim/lua/rimai/plugins/lualine.lua
+++ nvim/lua/rimai/plugins/lualine.lua
@@ -1,11 +1,11 @@
 require("lualine").setup({
-	options = { theme = 'sonokai' },
-	sections = {
-		lualine_a = { "mode" },
-		lualine_b = {'%{&ft == "toggleterm" ? "".b:toggle_number."" : ""}'},
+  options = { theme = 'sonokai' },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { 'filename' },
     lualine_c = {},
-		lualine_x = {},
-		lualine_y = { "branch", "diagnostics" },
-		lualine_z = { "location" },
-	},
+    lualine_x = {},
+    lualine_y = { "branch", "diagnostics" },
+    lualine_z = { "location" },
+  },
 })

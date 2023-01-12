290a90905799a9b73b4ec5cdd177ac74aa98f257
Remy de Jong <rimai4@hotmail.com>
2023-01-12 09:58:16 +0100

    remove double hover

--- nvim/lua/rimai/plugins/lsp.lua
+++ nvim/lua/rimai/plugins/lsp.lua
@@ -1,65 +1,64 @@
 local lsp = require("lsp-zero")
-local cmp = require('cmp')
+local cmp = require("cmp")
 
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  "tsserver",
-  "sumneko_lua",
-  "solargraph",
+	"tsserver",
+	"sumneko_lua",
+	"solargraph",
 })
 
 -- Fix undefined global 'vim'
 lsp.configure("sumneko_lua", {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
-    },
-  },
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
 })
 
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ["<CR>"] = nil,
+	["<CR>"] = nil,
 })
 
 lsp.setup_nvim_cmp({
-  mapping = cmp_mappings,
-  select_behavior = "insert",
-  preselect = cmp.PreselectMode.None,
-  sources = {
-    { name = "path" },
-    { name = "nvim_lsp", keyword_length = 1 },
-    { name = "buffer", keyword_length = 2 },
-    { name = "luasnip", keyword_length = 2 },
-  },
-  completion = {
-    completeopt = "menu,menuone,noselect"
-  }
+	mapping = cmp_mappings,
+	select_behavior = "insert",
+	preselect = cmp.PreselectMode.None,
+	sources = {
+		{ name = "path" },
+		{ name = "nvim_lsp", keyword_length = 1 },
+		{ name = "buffer", keyword_length = 2 },
+		{ name = "luasnip", keyword_length = 2 },
+	},
+	completion = {
+		completeopt = "menu,menuone,noselect",
+	},
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = { buffer = bufnr, remap = false }
+	local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 
-  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-  vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+	vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+	vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 
-  vim.keymap.set("n", "?", vim.lsp.buf.code_action, opts)
-  vim.keymap.set("n", "<leader>i", vim.lsp.buf.hover, opts)
-  vim.keymap.set("n", "<C-h>", vim.lsp.buf.hover, opts)
+	vim.keymap.set("n", "?", vim.lsp.buf.code_action, opts)
+	vim.keymap.set("n", "<leader>i", vim.lsp.buf.hover, opts)
 
-  -- disable mappings
-  vim.keymap.set("n", "K", "<C-u>zz", opts)
+	-- disable mappings
+	vim.keymap.set("n", "K", "<C-u>zz", opts)
 
-  vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format()' ]])
+	vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format()' ]])
 end)
 
 lsp.setup()
 
 vim.diagnostic.config({
-  signs = true,
+	signs = true,
 })

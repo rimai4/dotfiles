91263dfa2140e84d8bf9f429c0bad87c70ee825f
Remy de Jong <rimai4@hotmail.com>
2023-01-15 23:13:48 +0100

    enable indent

--- nvim/lua/rimai/plugins/treesitter.lua
+++ nvim/lua/rimai/plugins/treesitter.lua
@@ -1,64 +1,64 @@
 require("nvim-treesitter.configs").setup({
-	ensure_installed = {
-		"help",
-		"lua",
-		"javascript",
-		"typescript",
-		"ruby",
-		"tsx",
-		"css",
-		"scss",
-		"json",
-		"python",
-	},
-	sync_install = false,
-	auto_install = true,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	indent = {
-		enable = false,
-	},
-	endwise = {
-		enable = true,
-	},
-	autotag = {
-		enable = true,
-	},
-	textobjects = {
-		select = {
-			enable = true,
-			lookahead = true,
-			keymaps = {
-				["af"] = "@function.outer",
-				["if"] = "@function.inner",
-				["ac"] = "@class.outer",
-				["ic"] = "@class.inner",
-				["aa"] = "@parameter.outer",
-				["ia"] = "@parameter.inner",
-			},
-		},
-		swap = {
-			enable = true,
-			swap_next = {
-				["<leader>a"] = "@parameter.inner",
-			},
-			swap_previous = {
-				["<leader>A"] = "@parameter.inner",
-			},
-		},
-		move = {
-			enable = true,
-			set_jumps = true, -- whether to set jumps in the jumplist
-			goto_next_start = {
-				["]a"] = "@parameter.inner",
-				["]f"] = "@function.outer",
-			},
-			goto_previous_start = {
-				["[a"] = "@parameter.inner",
-				["[f"] = "@function.outer",
-			},
-		},
-	},
+  ensure_installed = {
+    "help",
+    "lua",
+    "javascript",
+    "typescript",
+    "ruby",
+    "tsx",
+    "css",
+    "scss",
+    "json",
+    "python",
+  },
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  indent = {
+    enable = true,
+  },
+  endwise = {
+    enable = true,
+  },
+  autotag = {
+    enable = true,
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+        ["aa"] = "@parameter.outer",
+        ["ia"] = "@parameter.inner",
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>a"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<leader>A"] = "@parameter.inner",
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]a"] = "@parameter.inner",
+        ["]f"] = "@function.outer",
+      },
+      goto_previous_start = {
+        ["[a"] = "@parameter.inner",
+        ["[f"] = "@function.outer",
+      },
+    },
+  },
 })

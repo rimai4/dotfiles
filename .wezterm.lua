7be8a1d26ccb21631221f595c530b117f92bf24a
Remy de Jong <rimai4@hotmail.com>
2023-01-26 00:13:33 +0100

    add resize, zoom & copy mode mappings

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,71 +1,97 @@
 local wezterm = require("wezterm")
 
 return {
-	font_size = 20,
-	window_decorations = "RESIZE",
-	force_reverse_video_cursor = true,
-	hide_tab_bar_if_only_one_tab = true,
-	color_scheme = "Sonokai (Gogh)",
-	font = wezterm.font("Liga SFMono Nerd Font"),
-	keys = {
-		{
-			key = "RightArrow",
-			mods = "CMD",
-			action = wezterm.action.SendKey({
-				key = "e",
-				mods = "CTRL",
-			}),
-		},
-		{
-			key = "LeftArrow",
-			mods = "CMD",
-			action = wezterm.action.SendKey({
-				key = "a",
-				mods = "CTRL",
-			}),
-		},
-		{
-			key = "Backspace",
-			mods = "CMD",
-			action = wezterm.action.SendKey({
-				key = "u",
-				mods = "CTRL",
-			}),
-		},
-		{
-			key = "w",
-			mods = "CMD",
-			action = wezterm.action.CloseCurrentPane({ confirm = true }),
-		},
-		{
-			key = "Enter",
-			mods = "CMD",
-			action = wezterm.action.SplitHorizontal,
-		},
-		{
-			key = "Enter",
-			mods = "CMD|SHIFT",
-			action = wezterm.action.SplitVertical,
-		},
-		{
-			key = "LeftArrow",
-			mods = "CMD|ALT",
-			action = wezterm.action.ActivatePaneDirection("Left"),
-		},
-		{
-			key = "RightArrow",
-			mods = "CMD|ALT",
-			action = wezterm.action.ActivatePaneDirection("Right"),
-		},
-		{
-			key = "UpArrow",
-			mods = "CMD|ALT",
-			action = wezterm.action.ActivatePaneDirection("Up"),
-		},
-		{
-			key = "DownArrow",
-			mods = "CMD|ALT",
-			action = wezterm.action.ActivatePaneDirection("Down"),
-		},
-	},
+  font_size = 20,
+  window_decorations = "RESIZE",
+  force_reverse_video_cursor = true,
+  hide_tab_bar_if_only_one_tab = true,
+  color_scheme = "Sonokai (Gogh)",
+  font = wezterm.font("Liga SFMono Nerd Font"),
+  keys = {
+    { key = "x", mods = "CMD", action = wezterm.action.ActivateCopyMode },
+    {
+      key = "Enter",
+      mods = "CMD",
+      action = wezterm.action.SplitHorizontal,
+    },
+    {
+      key = "Enter",
+      mods = "CMD|SHIFT",
+      action = wezterm.action.SplitVertical,
+    },
+    {
+      key = "w",
+      mods = "CMD",
+      action = wezterm.action.CloseCurrentPane({ confirm = true }),
+    },
+    {
+      key = "Backspace",
+      mods = "CMD",
+      action = wezterm.action.SendKey({
+        key = "u",
+        mods = "CTRL",
+      }),
+    },
+    {
+      key = "RightArrow",
+      mods = "CMD",
+      action = wezterm.action.SendKey({
+        key = "e",
+        mods = "CTRL",
+      }),
+    },
+    {
+      key = "LeftArrow",
+      mods = "CMD",
+      action = wezterm.action.SendKey({
+        key = "a",
+        mods = "CTRL",
+      }),
+    },
+    {
+      key = "LeftArrow",
+      mods = "CMD|ALT",
+      action = wezterm.action.ActivatePaneDirection("Left"),
+    },
+    {
+      key = "RightArrow",
+      mods = "CMD|ALT",
+      action = wezterm.action.ActivatePaneDirection("Right"),
+    },
+    {
+      key = "UpArrow",
+      mods = "CMD|ALT",
+      action = wezterm.action.ActivatePaneDirection("Up"),
+    },
+    {
+      key = "DownArrow",
+      mods = "CMD|ALT",
+      action = wezterm.action.ActivatePaneDirection("Down"),
+    },
+    {
+      key = "RightArrow",
+      mods = "SHIFT",
+      action = wezterm.action.AdjustPaneSize({ "Right", 8 }),
+    },
+    {
+      key = "LeftArrow",
+      mods = "SHIFT",
+      action = wezterm.action.AdjustPaneSize({ "Left", 8 }),
+    },
+    {
+      key = "UpArrow",
+      mods = "SHIFT",
+      action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+    },
+    {
+      key = "DownArrow",
+      mods = "SHIFT",
+      action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+    },
+    {
+      key = "z",
+      mods = "CMD",
+      action = wezterm.action.TogglePaneZoomState,
+    },
+  },
 }

43f7b7c14a94cd8b5805c6975a2008c871c3bb2f
Remy de Jong <rimai4@hotmail.com>
2023-01-27 13:07:07 +0100

    show relative path in lualine

--- nvim/lua/rimai/plugins/lualine.lua
+++ nvim/lua/rimai/plugins/lualine.lua
@@ -1,11 +1,16 @@
 return {
-	options = { theme = "sonokai" },
-	sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "filename" },
-		lualine_c = {},
-		lualine_x = {},
-		lualine_y = { "branch", "diagnostics" },
-		lualine_z = { "location" },
-	},
+  options = { theme = "sonokai" },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = {
+      {
+        "filename",
+        path = 1,
+      },
+    },
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = { "branch", "diagnostics" },
+    lualine_z = { "location" },
+  },
 }

2cc0b181d628c896064d8e9d4462e467c208eb26
Remy de Jong <rimai4@hotmail.com>
2023-03-13 15:28:37 +0100

    hlslens visual mode update

--- nvim/lua/rimai/plugins/hlslens.lua
+++ nvim/lua/rimai/plugins/hlslens.lua
@@ -20,6 +20,7 @@ vim.api.nvim_set_keymap(
   [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>zz]],
   kopts
 )
+vim.api.nvim_set_keymap("v", "*", [[*<Cmd>lua require('hlslens').start()<CR>N]], kopts)
 vim.api.nvim_set_keymap("n", "*", [[*<Cmd>lua require('hlslens').start()<CR>N]], kopts)
 vim.api.nvim_set_keymap("n", "#", [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
 vim.api.nvim_set_keymap("n", "g*N", [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)

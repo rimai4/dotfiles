169d0970c4e46c950f93279ceb17441e4de9b998
Remy de Jong <rimai4@hotmail.com>
2023-03-15 14:02:58 +0100

    disable italics

--- nvim/lua/rimai/colorscheme.lua
+++ nvim/lua/rimai/colorscheme.lua
@@ -1,4 +1,5 @@
 vim.cmd([[let g:sonokai_better_performance = 1]])
+vim.cmd([[let g:sonokai_disable_italic_comment = 1]])
 
 local status, _ = pcall(vim.cmd, "colorscheme sonokai")
 if not status then

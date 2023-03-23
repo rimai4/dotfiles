87c73767b422e69afce8d2519e75d7b02c61b06b
Remy de Jong <rimai4@hotmail.com>
2023-03-23 09:19:37 +0100

    gitui terminal highlight

--- nvim/lua/rimai/plugins/fterm.lua
+++ nvim/lua/rimai/plugins/fterm.lua
@@ -3,6 +3,7 @@ local fterm = require("FTerm")
 local gitui = fterm:new({
   ft = "fterm_gitui",
   cmd = "gitui",
+  hl = "BufferVisible",
   dimensions = {
     height = 0.95,
     width = 0.95,
@@ -10,6 +11,9 @@ local gitui = fterm:new({
 })
 
 -- Use this to toggle gitui in a floating terminal
+vim.keymap.set("t", "<A-g>", function()
+  gitui:toggle()
+end)
 vim.keymap.set("n", "<A-g>", function()
   gitui:toggle()
 end)

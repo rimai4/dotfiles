071f8e5995c966edf8d81facc94e0168108358da
Remy de Jong <rimai4@hotmail.com>
2023-03-24 11:39:34 +0100

    update font weight

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ return {
   force_reverse_video_cursor = true,
   hide_tab_bar_if_only_one_tab = true,
   color_scheme = "Sonokai (Gogh)",
-  font = wezterm.font("Liga SFMono Nerd Font", { weight = "Medium" }),
+  font = wezterm.font("Liga SFMono Nerd Font"),
   inactive_pane_hsb = {
     saturation = 0.8,
     brightness = 0.7,

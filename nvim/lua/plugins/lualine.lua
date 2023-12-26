7ae785352c07c188a495ba65b951fd1d5431ed54
Remy de Jong <rimai4@hotmail.com>
2023-12-26 09:27:29 +0100

    update lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -9,17 +9,19 @@ return {
 		return {
 			options = {
 				theme = "catppuccin",
+				section_separators = "",
+				component_separators = "",
 			},
 			sections = {
 				lualine_a = { "mode" },
-				lualine_b = {
+				lualine_b = { "branch" },
+				lualine_c = {
 					{
 						"filename",
 						path = 1,
 					},
 				},
-				lualine_c = {},
-				lualine_x = { "branch", "diagnostics" },
+				lualine_x = { "diagnostics" },
 				lualine_y = { cwd },
 				lualine_z = {},
 			},

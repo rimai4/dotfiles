15ce363ceacce0f3cdc55dc1a59307258b19bc0f
Remy de Jong <rimai4@hotmail.com>
2024-01-24 20:09:12 +0100

    add flash

---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,32 @@
+return {
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	opts = {
+		modes = {
+			char = {
+				enabled = false,
+			},
+			search = {
+				enabled = false,
+			},
+		},
+	},
+	keys = {
+		{
+			"m",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash",
+		},
+		{
+			"M",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").treesitter_search()
+			end,
+			desc = "Flash Treesitter",
+		},
+	},
+}

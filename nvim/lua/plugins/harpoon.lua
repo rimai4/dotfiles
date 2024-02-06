623fd1b79e6f0fb54f9f84b607b58b557e3f570d
Remy de Jong <rimai4@hotmail.com>
2024-02-06 20:22:29 +0100

    update harpoon mapping

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -7,7 +7,7 @@ return {
     -- stylua: ignore
 		return {
 			{ "<leader>ha", function() harpoon:list():append() end },
-			{ "<leader>hs", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end },
+			{ "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end },
 			{ "<leader>h1", function() harpoon:list():select(1) end },
 			{ "<leader>h2", function() harpoon:list():select(2) end },
 			{ "<leader>h3", function() harpoon:list():select(3) end },

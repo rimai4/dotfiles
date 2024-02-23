df466f9e54c28681d03e2cbb71a2a335dc0ce10e
Remy de Jong <rimai4@hotmail.com>
2024-02-23 21:36:30 +0100

    format on sav

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,14 +1,16 @@
 return {
 	"stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
 	opts = {
+		format_on_save = {},
 		formatters_by_ft = {
 			lua = { "stylua" },
 			python = { "ruff_fix", "ruff_format" },
 			javascript = { "prettierd" },
 			typescript = { "prettierd" },
 			typescriptreact = { "prettierd" },
+			scss = { "prettierd" },
 			json = { "prettierd" },
 			html = { "prettierd" },
 			go = { "gofmt", "goimports" },

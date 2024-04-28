d5bbc18a61d761fc01b493d899f61460246ff4e3
Remy de Jong <rimai4@hotmail.com>
2024-04-28 11:05:01 +0200

    don't source asdf

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
-
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH

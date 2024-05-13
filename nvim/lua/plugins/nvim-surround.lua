e3852d423dea027ea95ac7293a149d962d5e3b26
Remy de Jong <rimai4@hotmail.com>
2024-05-13 08:45:10 +0200

    replace sandwich with nvim-surround

---
+++ nvim/lua/plugins/nvim-surround.lua
@@ -0,0 +1,43 @@
+return {
+	"kylechui/nvim-surround",
+	version = "*",
+	keys = {
+		{ "sa", mode = { "n", "v" } },
+		{ "sr" },
+		{ "sd" },
+	},
+	opts = {
+		keymaps = {
+			normal = "sa",
+			visual = "sa",
+			delete = "sd",
+			change = "sr",
+		},
+		surrounds = {
+			["("] = {
+				add = { "(", ")" },
+			},
+			["{"] = {
+				add = { "{", "}" },
+			},
+			["<"] = {
+				add = { "<", ">" },
+			},
+			["["] = {
+				add = { "[", "]" },
+			},
+		},
+		highlight = {
+			duration = 150,
+		},
+		-- Alias cheatsheet (no changes from default)
+		aliases = {
+			["a"] = ">",
+			["b"] = ")",
+			["B"] = "}",
+			["r"] = "]",
+			["q"] = { '"', "'", "`" },
+			["s"] = { "}", "]", ")", ">", '"', "'", "`" },
+		},
+	},
+}

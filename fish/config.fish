5504f8c5c026a9b6b617a75193fdb8f1ee985f23
Remy de Jong <rimai4@hotmail.com>
2024-07-19 07:58:24 +0200

    fish.config

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,25 @@
+# Commands to run in interactive sessions can go in the block below.
+# Interactive session means a terminal window. Non-interactive sessions are run without user input, such as:
+# Cron jobs, scripts, docker entrypoints etc.
+# So put stuff here that is only required when running inside a terminal and not a script for example.
+# aliases, keybinds, environment variables that only affect terminal usage & setup for tools only used in the terminal
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+  # atuin
+  set -gx ATUIN_NOBIND "true"
+  atuin init fish | source
+  bind \cr _atuin_search
+  bind -M insert \cr _atuin_search
+
+  # zoxide
+  zoxide init fish | source
+
+  # python debugger
+  set -x PYTHONBREAKPOINT "ipdb.set_trace"
+
+  # asdf
+  source ~/.asdf/asdf.fish
 end
 
 # bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
-
-set --export PYTHONBREAKPOINT "ipdb.set_trace"
-
-source ~/.asdf/asdf.fish
+set -x BUN_INSTALL "$HOME/.bun"
+set -x PATH $BUN_INSTALL/bin $PATH

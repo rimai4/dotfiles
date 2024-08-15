9c7f0939b5d4b666cd2206a66d31397a49f47586
Remy de Jong <rimai4@hotmail.com>
2024-08-15 21:39:36 +0200

    command-line window

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -36,3 +36,18 @@ vim.cmd("autocmd BufRead,BufNewFile *.djhtml set filetype=htmldjango")
 
 -- Add templ filetype
 vim.filetype.add({ extension = { templ = "templ" } })
+
+-- Disable incremental selection in command-line window
+-- Check first if the keymap exists for the buffer before deleting
+vim.api.nvim_create_autocmd("CmdWinEnter", {
+	callback = function()
+		local keymaps = vim.api.nvim_buf_get_keymap(0, "n")
+
+		for _, keymap in ipairs(keymaps) do
+			if keymap.lhs == "<CR>" then
+				vim.api.nvim_buf_del_keymap(0, "n", "<CR>")
+				return
+			end
+		end
+	end,
+})

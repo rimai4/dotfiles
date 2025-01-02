1d3601ec5920e8f489ea2cff2d1e55882271310d
Remy de Jong <rimai4@hotmail.com>
2025-01-02 15:53:30 +0100

    center screen after search

--- .config/nvim/lua/plugins/hlslens.lua
+++ .config/nvim/lua/plugins/hlslens.lua
@@ -1,12 +1,12 @@
 return {
 	"kevinhwang91/nvim-hlslens",
-  keys = {
-    {"/"},
-    {"*"},
-    {"g*"},
-    {"#"},
-    {"g#"},
-  },
+	keys = {
+		{ "/" },
+		{ "*" },
+		{ "g*" },
+		{ "#" },
+		{ "g#" },
+	},
 	config = function()
 		require("hlslens").setup({
 			calm_down = true,
@@ -27,10 +27,10 @@ return {
 			[[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>zz]],
 			opts
 		)
-		vim.api.nvim_set_keymap("v", "*", [[*<Cmd>lua require('hlslens').start()<CR>N]], opts)
-		vim.api.nvim_set_keymap("n", "*", [[*<Cmd>lua require('hlslens').start()<CR>N]], opts)
-		vim.api.nvim_set_keymap("n", "g*", [[g*<Cmd>lua require('hlslens').start()<CR>N]], opts)
-		vim.api.nvim_set_keymap("n", "#", [[#<Cmd>lua require('hlslens').start()<CR>]], opts)
-		vim.api.nvim_set_keymap("n", "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]], opts)
+		vim.api.nvim_set_keymap("v", "*", [[*<Cmd>lua require('hlslens').start()<CR>Nzz]], opts)
+		vim.api.nvim_set_keymap("n", "*", [[*<Cmd>lua require('hlslens').start()<CR>Nzz]], opts)
+		vim.api.nvim_set_keymap("n", "g*", [[g*<Cmd>lua require('hlslens').start()<CR>Nzz]], opts)
+		vim.api.nvim_set_keymap("n", "#", [[#<Cmd>lua require('hlslens').start()<CR>zz]], opts)
+		vim.api.nvim_set_keymap("n", "g#", [[g#<Cmd>lua require('hlslens').start()<CR>zz]], opts)
 	end,
 }

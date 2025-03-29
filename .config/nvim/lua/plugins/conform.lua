0232dc5b6d7f9af28026d2b9be599cf46d527652
Remy de Jong <rimai4@hotmail.com>
2025-03-29 17:41:46 +0100

    add FormatDisable/Enable

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,15 +3,19 @@ return {
 	event = { "BufWritePre" },
 	cmd = { "ConformInfo" },
 	opts = {
-		format_on_save = {
-			timeout_ms = 3000,
-			lsp_fallback = false,
-		},
+		format_on_save = function(bufnr)
+			-- Disable with a global or buffer-local variable
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
+			return { timeout_ms = 500, lsp_format = "fallback" }
+		end,
 		formatters_by_ft = {
 			lua = { "stylua" },
 			python = { "ruff_fix", "ruff_format" },
 			javascript = { "prettierd" },
 			typescript = { "prettierd" },
+			css = { "prettierd" },
 			typescriptreact = { "prettierd" },
 			scss = { "prettierd" },
 			json = { "prettierd" },
